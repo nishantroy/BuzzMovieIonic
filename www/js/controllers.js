4205d9b6bf60123359e047cb133af8dffe58e46d
Nishant Roy <nroy6@gatech.edu>
2016-04-24 05:06:19 -0400

    User Ratings saved and recalled

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', ['ionic', 'firebase', 'ui.router'])
+angular.module('starter.controllers', ['ionic', 'firebase', 'ui.router', 'ionic.rating'])
 
   .controller('SearchCtrl',
     function ($scope, $http, Movie, $state) {
@@ -6,52 +6,80 @@ angular.module('starter.controllers', ['ionic', 'firebase', 'ui.router'])
       $scope.data = {};
 
       $scope.searchMovie = function () {
-        var url1 = "https://www.omdbapi.com/?t=";
-        var url2 = "&y=&plot=full&r=json";
 
-        var url = url1 + $scope.data.moviename + url2;
-        var flag = false;
+        if ($scope.data.moviename == undefined || $scope.data.moviename.trim() == "") {
+          alert("Please enter a movie name");
+        } else {
+          var url1 = "https://www.omdbapi.com/?t=";
+          var url2 = "&y=&plot=full&r=json";
 
-        $http.get(url)
-          .success(function (data) {
-            // console.log(data);
-            Movie.moviedata = data;
-            if (data.Response == 'True') {
-              flag = true;
-            } else {
-              console.log("Movie not found!");
-              alert("Movie not found!");
-            }
-          })
-          .error(function (data) {
-            console.log("ERROR");
-          })
-          .finally(function () {
-            if (flag) {
-              $state.transitionTo('movieDetails');
-            }
-          });
+          var url = url1 + $scope.data.moviename + url2;
+          var flag = false;
 
+          $http.get(url)
+            .success(function (data) {
+              // console.log(data);
+              Movie.moviedata = data;
+              if (data.Response == 'True') {
+                flag = true;
+              } else {
+                console.log("Movie not found!");
+                alert("Movie not found!");
+              }
+            })
+            .error(function (data) {
+              console.log("ERROR");
+            })
+            .finally(function () {
+              if (flag) {
+                $state.transitionTo('movieDetails');
+              }
+            });
+        }
       }
     })
 
   .controller('MovieCtrl',
-    function ($scope, Movie, $state) {
+    function ($scope, Movie, $state, AuthData, UsersRef) {
       // $scope.$on('$ionicView.enter', function () {
       $scope.input = Movie.moviedata;
       console.log($scope.input);
+      var authData = AuthData;
+      console.log(authData);
+      var ref = UsersRef.child(authData.uid).child("Movies").child($scope.input.Title);
 
-      // });
+      $scope.rating = {};
+      $scope.rating.rate = 0;
+      $scope.rating.max = 10;
+
+      ref.once("value", function (snapshot) {
+        if (snapshot.exists()) {
+          console.log("Found!");
+
+          $scope.$apply(function() {
+            $scope.rating.rate = snapshot.child("Rating").val();
+          })
+        }
+      });
+
+
+
+// });
 
       $scope.goBack = function () {
+        console.log($scope.rating.rate);
+        ref.update({
+          Rating: $scope.rating.rate
+        });
         $state.transitionTo('tab.search')
       }
 
     })
 
-  .controller('CreateAccountCtrl', ["$scope", "Auth", "$state", "$ionicHistory",
-    function ($scope, Auth, $state, $ionicHistory) {
+  .controller('CreateAccountCtrl', ["$scope", "Auth", "UsersRef", "$state", "$ionicHistory",
+    function ($scope, Auth, UsersRef, $state, $ionicHistory) {
       $scope.data = {};
+      var ref = UsersRef;
 
       $scope.createUser = function () {
         $scope.message = null;
@@ -66,7 +94,13 @@ angular.module('starter.controllers', ['ionic', 'firebase', 'ui.router'])
           password: $scope.data.password
         }).then(function (userData) {
           $scope.message = "User created with uid: " + userData.uid;
+          var user = {email: $scope.data.email};
+          var obj = {};
+          obj[userData.uid] = user;
           console.log("Message: " + $scope.message + "\n");
+          ref.child(userData.uid).set({
+            email: $scope.data.email
+          });
           $state.transitionTo('login');
         }).catch(function (error) {
           $scope.error = error;
@@ -119,9 +153,8 @@ angular.module('starter.controllers', ['ionic', 'firebase', 'ui.router'])
       $scope.checkData = function () {
         if ($scope.data.email == undefined) {
           alert("Invalid Email!")
-        } else if ($scope.data.password == undefined || $scope.data.password.trim() == ""
-          || $scope.data.password.length < 8) {
-          alert("Please enter a password at least 8 characters long!")
+        } else if ($scope.data.password == undefined || $scope.data.password.trim() == "") {
+          alert("Please enter a password !")
         } else {
           console.log("Email: " + $scope.data.email + " \n Password: " + $scope.data.password);
           $scope.loginUser();
